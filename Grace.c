7f816755e4ecc2965280b0dff817f599229108fa
Jukino <acazuc@student.42.fr>
2016-05-17 21:09:36 +0200

    Finished Grace

--- Grace.c
+++ Grace.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<fcntl.h>
 #include<unistd.h>
-#define RUN() (main())
-static void print(int fd){char*s="#include<stdio.h>%c#include<fcntl.h>%c#include<unistd.h>%c#define RUN() (main())%cstatic void print(int fd){char*s=%c%s%c;dprintf(fd,s,10,10,10,10,34,s,34,10,34,34,10);}%cint RUN(){int fd;if((fd=open(%cGrace_kid.c%c,O_WRONLY|O_CREAT|O_TRUNC,0644))==-1){return(0);}print(fd);close(fd);}%c";dprintf(fd,s,10,10,10,10,34,s,34,10,34,34,10);}
-int RUN(){int fd;if((fd=open("Grace_kid.c",O_WRONLY|O_CREAT|O_TRUNC,0644))==-1){return(0);}print(fd);close(fd);}
+/*
+	such comment
+*/
+#define LI "#include<stdio.h>%c#include<fcntl.h>%c#include<unistd.h>%c/*%c	such comment%c*/%c#define LI %c%s%c%c#define FD int fd;if((fd=open(%cGrace_kid.c%c,O_WRONLY|O_CREAT|O_TRUNC,0644))==-1){return(0);}%c#define FT int main(){FD print(fd);close(fd);}%cstatic void print(int fd){char*s=LI;dprintf(fd,s,10,10,10,10,10,10,34,s,34,10,34,34,10,10,10,10);}%cFT%c"
+#define FD int fd;if((fd=open("Grace_kid.c",O_WRONLY|O_CREAT|O_TRUNC,0644))==-1){return(0);}
+#define FT int main(){FD print(fd);close(fd);}
+static void print(int fd){char*s=LI;dprintf(fd,s,10,10,10,10,10,10,34,s,34,10,34,34,10,10,10,10);}
+FT
