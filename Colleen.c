7858e37e13529b9cd6c736cad04d6ce319c342cf
Jukino <acazuc@student.42.fr>
2016-05-17 20:59:14 +0200

    Fixed comments

--- Colleen.c
+++ Colleen.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-//le commentaire
-static void print(char*s){printf(s,10,47,10,10,10,47,10,34,s,34,10);}
+/*
+	le commentaire
+*/
+static void print(char*s){printf(s,10,10,10,10,10,10,10,10,10,34,s,34,10);}
 int main(){
-//le commentaire
-char*s="#include<stdio.h>%c%c/le commentaire%cstatic void print(char*s){printf(s,10,47,10,10,10,47,10,34,s,34,10);}%cint main(){%c%c/le commentaire%cchar*s=%c%s%c;print(s);}%c";print(s);}
+/*
+	le commentaire
+*/
+char*s="#include<stdio.h>%c/*%c	le commentaire%c*/%cstatic void print(char*s){printf(s,10,10,10,10,10,10,10,10,10,34,s,34,10);}%cint main(){%c/*%c	le commentaire%c*/%cchar*s=%c%s%c;print(s);}%c";print(s);}
