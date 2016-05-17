a47642d9686118098ac807d7f12dee0f1ddb510b
Jukino <acazuc@student.42.fr>
2016-05-17 19:38:16 +0200

    Finished Colleen

--- Colleen.c
+++ Colleen.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 //le commentaire
+static void print(char*s){printf(s,10,47,10,10,10,47,10,34,s,34,10);}
 int main(){
 //le commentaire
-char*s="#include<stdio.h>%c%c/le commentaire%cint main(){%c%c/le commentaire%cchar*s=%c%s%c;printf(s,10,47,10,10,47,10,34,s,34,10);}%c";printf(s,10,47,10,10,47,10,34,s,34,10);}
+char*s="#include<stdio.h>%c%c/le commentaire%cstatic void print(char*s){printf(s,10,47,10,10,10,47,10,34,s,34,10);}%cint main(){%c%c/le commentaire%cchar*s=%c%s%c;print(s);}%c";print(s);}
